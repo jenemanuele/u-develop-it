1ef321352251c28f2f8ca1045844c9f53089dca0
Jennifer Emanuele <jemanuele@gmail.com>
2022-01-27 19:22:30 -0500

    started server w/ sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
-DROP TABLE IF EXISTS voters;
+-- DROP TABLE IF EXISTS candidates;
+-- DROP TABLE IF EXISTS parties;
+-- DROP TABLE IF EXISTS voters;
 
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
+-- CREATE TABLE parties (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL,
+--   description TEXT
+-- );
 
 
 CREATE TABLE candidates (
@@ -16,10 +16,3 @@ CREATE TABLE candidates (
     industry_connected BOOLEAN NOT NULL
 );
 
-CREATE TABLE voters (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
